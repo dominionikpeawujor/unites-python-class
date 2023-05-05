99cdf668e71b0b84b11789a2854a84200b20450f
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-05-05 14:46:21 +0100

    Initial Commit

---
+++ DAY 1/day-1/day-1.py
@@ -0,0 +1,86 @@
+# === Common Command Line statements ===
+## == Windows ==
+
+# - cd - Change Directory
+# ----- cd <Name of the director/folder>
+# ----- cd .. - Going a step backwards
+# ----- cd / - Going to root directory
+
+# - dir - Listing Directory
+
+# - mkdir - Make Directory
+# ----- mkdir <Name the folder>
+
+## == MacOS == 
+# - cd - Change Directory
+# ----- cd <Name of the director/folder>
+# ----- cd .. - Going a step backwards
+# ----- cd ~ - Going to root directory
+
+# ls - List Directory
+# ls -a - list all
+
+# - mkdir - Make Directory
+# ----- mkdir <Name the folder>
+
+# ======================= PYTHON =======================
+
+# Basic Syntax
+# print('Hello World')
+# print("Hello World")
+
+# print(2/2, 'Division')
+
+# ============== Variables ==============
+# Containers that stores data values
+number1 = 2
+number2 = 2
+
+number1 = 25
+print(number1, 'Reassign value to number1')
+
+
+print(number1 + number2, 'Addition')
+
+statements = 'Hello World'
+print(statements)
+
+# ============== Naming Conventions ==============
+# Do not start variable names with numbers
+
+# Do not start with symbols except an underscore
+_mount = 23
+
+# Always write meaningful varaiable names
+
+x = 19 # Not advised
+y = 15 # Not advised
+
+# Do not add spaces within your variable names
+
+# ============= Naming Styles ============= 
+
+# PascalCase - Used to define Classes
+TotalAmount = 10
+TotalAverageOfStudents = 1000
+
+# camelCase - used to define variable and functions
+totalAmount = 200
+totalAverageOfStudents = 2000
+
+# snake_case - mostly variables
+dollar_amount = 10000
+
+#To comment in visual studio, highlight the code and use the Ctrl + / (Windows) or Cmd + / (MacOS)
+
+# ====== Input & Output Operations ======
+fullName = input("Please enter your name: ")
+profession = input('What do you do? ')
+location = input('Where are you currently? ')
+
+# Concatenation - Joining two or more strings to form one string
+print('Hello! ' + fullName) 
+print('Profession: ' + profession)
+print('Location: ' + location)
+
+
